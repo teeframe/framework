f7ca367475b157eb901c296e5070dd51ac0f6752
miguilimzero <35383529+miguilimzero@users.noreply.github.com>
2026-06-21 17:33:09 -0300

    Update AbstractMapItem.php

--- map/MapItems/AbstractMapItem.php
+++ map/MapItems/AbstractMapItem.php
@@ -4,26 +4,7 @@ namespace TeeFrame\Map\MapItems;
 
 abstract class AbstractMapItem
 {
-    public function __construct(
-    ) {
-    }
-
-    // public function __construct(
-    //     protected int $id,
-    //     protected int $size,
-    //     protected string $data,
-    // ) {
-    // }
+    public function __construct() {}
 
     abstract public static function make(int $id, int $size, string $data): static;
-
-    // public function getId(): int
-    // {
-    //     return $this->id;
-    // }
-
-    // public function getSize(): int
-    // {
-    //     return $this->size;
-    // }
 }
